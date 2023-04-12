07ffd59c4c6114a14cf1a0167f7106f730595805
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-12 18:04:21 -0500

    Added errorLogger File and Date conversion logic

--- src/daysLeft.js
+++ src/daysLeft.js
@@ -1,12 +1,13 @@
+import convertDate from './convertDate.js';
+
 export default function daysLeft(dueDate) {
 	let curDate = [
 		`${new Date().getUTCMonth()}`,
 		`${new Date().getUTCDate()}`,
 		`${new Date().getUTCFullYear()}`,
 	];
-	convertDate;
-
+	convertDate(curDate);
 	dueDate = '12 / 12 / 2023';
-	let daysRemaining = dueDate - curDate;
-	console.log(dueDate, curDate, daysRemaining);
+	// let daysRemaining = dueDate - curDate;
+	// console.log(dueDate, curDate, daysRemaining);
 }
