0da739b8fa76b12b8ab00f82a5207215360d842d
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-13 20:38:39 -0500

    Added Date Error handling

--- src/dateHandling/convertDate.js
+++ src/dateHandling/convertDate.js
@@ -10,9 +10,16 @@ export default function convertDate() {
 }
 
 export function convertDueDate(dueDate) {
-	if (typeof dueDate != 'object') {
-		errorLog(`${dueDate} is not a valid entry`);
+	if (
+		typeof dueDate != 'object' ||
+		dueDate[0] > 12 ||
+		dueDate[0] < new Date().getMonth() + 1 ||
+		(dueDate[1] < new Date().getDate() &&
+			dueDate[0] == new Date().getMonth() + 1) ||
+		dueDate[2] < new Date().getFullYear()
+	) {
+		return errorLog(`${dueDate} is not a valid entry`);
+	} else {
+		return { dueDate };
 	}
-	// better error handling
-	return { dueDate };
 }
