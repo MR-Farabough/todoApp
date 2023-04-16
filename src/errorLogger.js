6b19c9236741a8d3418ec24b969ef28bd2d4caf2
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-16 18:11:58 -0500

    Changed /docs folder to /dist

--- src/errorLogger.js
+++
@@ -1,3 +0,0 @@
-export default function errorLog(message) {
-	console.error(message);
-}
--- src/storage/saveToText.js
+++
@@ -1,7 +0,0 @@
-export default function saveToText() {
-	const a = document.createElement('a');
-	const blob = new Blob([localStorage.getItem('storage-array')]);
-	a.href = URL.createObjectURL(blob);
-	a.download = 'Storage.txt';
-	a.click();
-}
