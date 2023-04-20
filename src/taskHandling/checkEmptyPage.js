eef6f6a98ef308fe22cb23057fc1fbb53bcf160d
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-19 23:11:28 -0500

    added random image for blank page

--- src/taskHandling/checkEmptyPage.js
+++ src/taskHandling/checkEmptyPage.js
@@ -4,8 +4,20 @@ export default function checkEmpty() {
 		document.querySelector('.quote').textContent == 'Total Tasks: 0' &&
 		cardsEL.querySelector('img') == null
 	) {
+		const arrOfImages = [
+			'imgs/cat.png',
+			'imgs/cat1.png',
+			'imgs/cuteDog.png',
+			'imgs/dog-watermelon.png',
+			'imgs/dog1.png',
+			'imgs/dog2.png',
+			'imgs/dogYawn.png',
+			'imgs/hampster.png',
+		];
+		const number = Math.floor(Math.random() * arrOfImages.length);
+		console.log(number);
 		const img = new Image();
-		img.src = 'imgs/cat.png';
+		img.src = arrOfImages[number];
 		img.style.height = '35%';
 		img.style.width = '35%';
 		img.style.minHeight = '300px';
