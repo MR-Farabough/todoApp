a1a421833d84a3f271da23d29c54215d814f1857
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-24 15:34:32 -0500

    memory issue fix #2

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import renderCards from './taskHandling/renderCards.js';
 import updateTaskCount from './taskHandling/taskCount.js';
 
 // Update due dates, time, and render cards
-if (JSON.parse(localStorage.getItem('storage-array').length) == null) {
+if (JSON.parse(localStorage.getItem('storage-array')) == null) {
 	localStorage.setItem('storage-array', '[]');
 }
-if (JSON.parse(localStorage.getItem('deleted-array').length) == null) {
+if (JSON.parse(localStorage.getItem('deleted-array')) == null) {
 	localStorage.setItem('storage-array', '[]');
 }
 getOverdueDays(JSON.parse(localStorage.getItem('storage-array')));
